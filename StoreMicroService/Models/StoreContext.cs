642f172313541d090401e43c6e9be5f1e5aafeef
Szado12 <michal.pijanowski2@gmail.com>
2023-01-28 19:29:28 +0100

    Connection string update

--- StoreMicroService/Models/StoreContext.cs
+++ StoreMicroService/Models/StoreContext.cs
@@ -27,10 +27,19 @@ public partial class StoreContext : DbContext
     public virtual DbSet<WoodType> WoodTypes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=PSI_SawMill;Trusted_Connection=True;Trust Server Certificate=true");
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        IConfiguration configuration = new ConfigurationBuilder()
+          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+          .AddJsonFile("appsettings.json")
+          .Build();
+        var connectionString = configuration["ConnectionString"];
+        optionsBuilder.UseSqlServer(connectionString);
+      }
+    }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Address>(entity =>
         {
